6e7afba1f40c71f9b90840e31764e5d89c476611
Juan <juanmrodriguez1989@gmail.com>
2020-09-23 19:06:10 -0300

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+// Creando nuestro propio hook
+
+function useCuenta(init, max) {
+  /* esto seria un hook y usa la palabra reservada use*/
+  const [cuenta, setCuenta] = React.useState(init);
+  if (cuenta > max) {
+    setCuenta(0);
+  }
+  return [cuenta, setCuenta];
+}
+
+// Usando el hook useState
 function App() {
+  const [cuenta, setCuenta] = useCuenta(0, 5);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setCuenta(cuenta + 1)}>Sumar: {cuenta} </button>
   );
 }
 
